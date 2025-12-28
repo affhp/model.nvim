5939e8217571c44bcac29cd799ea45760da5f7ce
affhp <175476148+affhp@users.noreply.github.com>
2025-12-28 17:45:53 +0000

    Fix llamacpp provider
    When server started, the recent version of llama-server log message has changed from old `HTTP server listening` to new `server is listening on`. The plugin failed to find the old pattern to determine whether the server has started or not.

--- lua/model/providers/llamacpp.lua
+++ lua/model/providers/llamacpp.lua
@@ -49,7 +49,7 @@ local function start_server(model, args, on_started)
   local sys_opts = resolve_system_opts(model, args or {})
 
   local stop = system(sys_opts.cmd, sys_opts.args, {}, function(out)
-    if out and out:find('HTTP server listening') then
+    if out and out:find('server is listening on') then
       util.show('llama.cpp server started')
       on_started()
     end

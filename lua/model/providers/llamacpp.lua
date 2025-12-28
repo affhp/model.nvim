6e6d64ed1c6780c719518d65327d41829d8fb823
affhp <175476148+affhp@users.noreply.github.com>
2025-12-28 19:36:12 +0000

    test4

--- lua/model/providers/llamacpp.lua
+++ lua/model/providers/llamacpp.lua
@@ -49,7 +49,7 @@ local function start_server(model, args, on_started)
   local sys_opts = resolve_system_opts(model, args or {})
 
   local stop = system(sys_opts.cmd, sys_opts.args, {}, function(out)
-    if out then
+    if string.find(out, "listening on") then
       util.show('llama.cpp server started')
       on_started()
     end
